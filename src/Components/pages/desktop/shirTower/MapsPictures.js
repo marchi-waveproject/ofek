d2eff15890e5fa4b5312ed5c7c47da0022c4c448
marchi-waveproject <daniel@waveproject.com>
2023-11-06 12:35:50 +0200

    co

--- src/Components/pages/desktop/shirTower/MapsPictures.js
+++ src/Components/pages/desktop/shirTower/MapsPictures.js
@@ -1,85 +1,98 @@
-import React, { Component } from 'react'
-import Container from '@material-ui/core/Container';
+import React, { Component } from "react";
+import Container from "@material-ui/core/Container";
 import APPIpic from "./../../../APPIpic";
 
-import lupa from './../../../../img/icons/white/lupa.svg'
-import PopupPicture from './PopupPicture';
+import lupa from "./../../../../img/icons/white/lupa.svg";
+import PopupPicture from "./PopupPicture";
 import Owl from "./.././../../Owl";
 
 export default class MapsPictures extends Component {
+  constructor(props) {
+    super(props);
 
+    //console.log(this.props);
 
-    constructor(props) {
+    this.state = {
+      popUpText_View: false,
+      popUpText_Message: "הודעה",
+    };
+  }
 
-        super(props);
-        
-        //console.log(this.props);
+  showHidepopUpText = (longText) => {
+    //console.log(showMe);
 
-        this.state = {
-            popUpText_View:  false,
-            popUpText_Message: 'הודעה'
-        }
-    }
+    this.setState({
+      popUpText_View: true,
+      popUpText_Message: longText,
+    });
 
-    showHidepopUpText = (longText) => {
+    console.log(this.state.popUpText_View);
+  };
 
-        //console.log(showMe);
+  hidePopUp = () => {
+    this.setState({
+      popUpText_View: false,
+    });
+    //console.log('DONE');
+  };
 
-        this.setState(
-            {
-                popUpText_View : true,
-                popUpText_Message: longText
-            });
+  render() {
+    let page = this.props.page;
 
-        console.log(this.state.popUpText_View);
-        
-        
-    }
+    return (
+      <React.Fragment>
+        <Container maxWidth="lg">
+          <h2 className="">{page.MapsPictures_h2}</h2>
 
-    hidePopUp = () => {
-        this.setState(
-            {
-                popUpText_View : false
-            }
-        )
-        //console.log('DONE');
-    }
+          {this.props.mobile ? (
+            <div className="picturesMobile">
+              <Owl
+                pictures={this.props.gallery}
+                section1={true}
+                config={{
+                  mobile: true,
+                  items: 1,
+                  stagePadding: 40,
+                  margin: 5,
+                  nav: false,
+                  dots: false,
+                }}
+              />
+            </div>
+          ) : (
+            <div className="pictures">
+              {this.props.gallery.map((item) => (
+                <div
+                  className="item"
+                  key={item.data.id}
+                  onClick={() => this.showHidepopUpText(item.data.image_big)}
+                >
+                  <APPIpic
+                    showclass="mainPic"
+                    pic={item}
+                    mobile={this.props.mobile}
+                  />
+                  <div className="hover">
+                    {item.data.alt}
+                    <img className="lupa" src={lupa} alt="" />
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </Container>
 
-
-    render() {
-
-        let page = this.props.page;
-
-        return (
-            <React.Fragment>
-                <Container maxWidth="lg" >
-                    <h2 className=''>{page.MapsPictures_h2}</h2>
-
-
-                    { this.props.mobile ? 
-                        <div className='picturesMobile'>
-                            < Owl pictures={this.props.gallery} section1={true} config={{mobile:true, items: 1, stagePadding:40, margin: 5, nav: false, dots: false}} />
-                        </div> :
-
-                        <div className='pictures'>
-                            {
-                                this.props.gallery.map(item => (
-                                <div className="item" key={item.data.id}  onClick ={ () => this.showHidepopUpText(item.data.image_big) }>
-                                    <APPIpic  showclass="mainPic" pic={item} mobile={this.props.mobile} />
-                                    <div className="hover">
-                                        {item.data.alt}
-                                        <img className="lupa" src={lupa} alt="" />
-                                    </div>
-                                </div>    
-                                ))
-                            } 
-                        </div>
-                    }
-                </Container>
-
-                {this.state.popUpText_View ? <PopupPicture hidePopUp={this.hidePopUp} pageData={page} show={true} message={this.state.popUpText_Message} /> : false }
-
-            </React.Fragment>
-        )
-    }
+        {this.state.popUpText_View ? (
+          <PopupPicture
+            hidePopUp={this.hidePopUp}
+            pageData={page}
+            show={true}
+            message={this.state.popUpText_Message}
+          />
+        ) : (
+          false
+        )}
+      </React.Fragment>
+    );
+  }
 }
